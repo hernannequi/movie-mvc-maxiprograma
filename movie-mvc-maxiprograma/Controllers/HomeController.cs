b9dc4d92d7cbb32dc8817559fecf295fb248ac87
hernannequi <davidhernannequi@gmail.com>
2026-04-18 22:34:25 -0300

    commit inicial

---
+++ movie-mvc-maxiprograma/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using movie_mvc_maxiprograma.Models;
+
+namespace movie_mvc_maxiprograma.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ movie-mvc-maxiprograma/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace movie_mvc_maxiprograma.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
